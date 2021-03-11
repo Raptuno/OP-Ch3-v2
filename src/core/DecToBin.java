8c8147a1f344d654542749a6f1de38bad8a7ed5a
Raptuno <65292196+Raptuno@users.noreply.github.com>
2021-03-11 10:11:31 -0600

    Initial commit

---
+++ src/core/DecToBin.java
@@ -0,0 +1,16 @@
+package core;
+import java.io.*;
+
+public class DecToBin {
+    /* Pasos para convertir decimal a binario
+    1. Dividir el número entre 2
+    2. Obtener el cociente de la división
+    3. Obtener el residuo de la división
+    4. Repetir hasta que el cociente sea 0 (cero)
+     */
+    public String dtb(String input){
+        int ipt=Integer.parseInt(input);
+        String output=new String();
+        return output;
+    }
+}
---
+++ src/core/Main.java
@@ -0,0 +1,8 @@
+package core;
+import java.io.*;
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hola Mundo");
+    }
+}
